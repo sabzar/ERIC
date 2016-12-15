a71c66a4eca2331e9025ffdb269ebdba2a4adaea
unknown <corvus5e@gmail.com>
2016-12-15 23:52:56 +0200

    who commits ?

--- Launcher.bat
+++ Launcher.bat
@@ -1,3 +1,4 @@
 cd /d D:\git\ERIC
 ERIC\Debug\ERIC.exe -file scans\D-2.txt -eps 80 -minPts 0 -min_rdp_eps 20 -max_dist 100 -min_part_size 8 -merge_dist 50 -merge_angle 20 -filter_kN 10 -filter_threashold 0.9
+
 exit
