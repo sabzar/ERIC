c850304793650ce305f1caae43c1c5579b26c1e5
unknown <corvus5e@gmail.com>
2016-12-18 12:57:08 +0200

    test branching

--- Launcher.bat
+++ Launcher.bat
@@ -1,4 +1,4 @@
 cd /d D:\git\ERIC
-ERIC\Debug\ERIC.exe -file scans\D-10.txt -eps 80 -minPts 5 -min_rdp_eps 15 -max_dist 1000 -min_part_size 1 -merge_dist 50 -merge_angle 20 -filter_kN 10 -filter_threashold 0.9 -reduce_median_window 20
-
+ERIC\Debug\ERIC.exe -file scans\D-17.txt -eps 80 -minPts 5 -min_rdp_eps 15 -max_dist 100 -min_part_size 8 -merge_dist 50 -merge_angle 20 -filter_kN 10 -filter_threashold 0.9 -reduce_median_window 20
+REM this is new branch "new_filter"
 exit
